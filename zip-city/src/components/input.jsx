6c1dd992b35716e60ce7d1cb9f99dbec17064ac2
jons-repo <s.jonathan4252@gmail.com>
2023-06-16 14:30:38 -0400

    Worked on input file, added textbox and function that returns city from a zipcode.

---
+++ zip-city/src/components/input.jsx
@@ -0,0 +1,40 @@
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+
+const Input = () => {
+    const [zipCode, setZipCode] = useState("");
+    const [placeName, setPlaceName] = useState("");
+
+    const Button = () => {
+        return <button>Search</button>;
+    }
+    
+    // 
+    useEffect(() => {
+        async function getZipCode() {
+            try {
+                const placeName = await axios.get(`https://zip-api.eu/api/v1/info/US-${[11371]}`);
+                console.log(placeName);
+                setPlaceName(placeName.data);
+                
+            } catch(error) {
+                console.error(error);
+            }
+        }
+        getZipCode();
+    }, []);
+
+    return (
+        <div>
+            <input type="text"/>
+            {/* Looping through JSON */}
+            <p key={placeName.url}>{placeName.place_name}</p>
+            {/* {placeName.data.map(placeName => {
+                return <p key={placeName.url}>{placeName.place_name}</p>
+            })
+            } */}
+        </div>
+    );
+};
+
+export default Input;
