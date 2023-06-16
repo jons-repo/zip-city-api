a0147e02d2acc73e5de407b4c4244d5911f1a9dd
Bruno Ogbennaya <bruno.ogbennaya@gmail.com>
2023-06-16 15:52:10 -0400

    Bruno's commit

--- zip-city/src/components/input.jsx
+++ zip-city/src/components/input.jsx
@@ -1,50 +1,31 @@
-import React, {useEffect, useState} from "react";
+/*This Program accepts zipcode and returns city */
+import React, {useState} from "react";
 import axios from "axios";
 
 const Input = () => {
-    // useState for input before we use it to change zipCode
-    const [userInput, setUserInput] = useState("");
     const [zipCode, setZipCode] = useState("");
     const [placeName, setPlaceName] = useState("");
 
-    // const Button = () => {
-    //     return ;
-    // }
-    
-    // 
-    useEffect(() => {
-        async function getZipCode() {
-            try {
-                setZipCode(userInput);
-                const placeName = await axios.get(`https://zip-api.eu/api/v1/info/US-${[zipCode]}`);
-                console.log(placeName);
-                setPlaceName(placeName.data);
-                
-            } catch(error) {
-                console.error(error);
-            }
+    async function getPlaceName() {
+        try {
+            const placeName = await axios.get(`https://zip-api.eu/api/v1/info/US-${zipCode}`);
+            setPlaceName(placeName.data);
+            
+        } catch(error) {
+            console.error(error);
         }
-        getZipCode();
-    }, []);
+    }
 
-    /* search() {
-        const userInput = 
-        setUserInput(userInput);
-        setZipCode(userInput);
-    };
-    */
-
+    const handleChange = (event) => {
+        setZipCode(event.target.value);
+    }
+    
     return (
-        <div> 
-            <input value={userInput} onInput={e => setUserInput(e.target.value)}/>
-            { /*<button onchange={e => setZipCode(e.target.value)}>Search</button> */}
-            {/* Looping through JSON */}
-            <p key={placeName.url}>{placeName.place_name}</p>
-            {/* {placeName.data.map(placeName => {
-                return <p key={placeName.url}>{placeName.place_name}</p>
-            })
-            } */}
-        </div>
+      <div>
+        <input value={zipCode} onChange={handleChange} />
+        <button onClick={getPlaceName}>Search</button>
+        <p key={placeName.url}>{placeName.place_name}</p>
+      </div>
     );
 };
 
